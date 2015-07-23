95f7fa996981d3689a4904cadfd9f006feb496d2
klaslofstedt <klaslofstedt22@gmail.com>
2015-07-23 18:55:17 +0200

    first commit

---
+++ inc/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: 10 jul 2012
+ *      Author: BenjaminVe
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+// Function prototypes
+//void timing_handler();
+__IO uint32_t msTicks;
+#endif /* MAIN_H_ */
---
+++ inc/variables.h
@@ -0,0 +1,8 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+__IO uint32_t msTicks;
+
+void Delay (__IO uint32_t dlyTicks);
+
+#endif
